eeacf6c60a242516084585b306c3623170284c5f
Cam Pedersen <diffference@gmail.com>
2012-02-21 17:13:53 -0500

    [fix] >_>

--- examples/pong.js
+++ examples/pong.js
@@ -2,6 +2,6 @@ var Nucleus = require('../');
 var nucleus = new Nucleus();
 
 nucleus.on('ping', function (data) {
-  this.emit('pong', { foo: 'bar' });
+  nucleus.emit('pong', { foo: 'bar' });
 });
 
